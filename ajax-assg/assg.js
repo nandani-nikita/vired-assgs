c3c19c13b854c0d20efd4dc3b74c530cae6e14b6
Nikita <nikitann2501@gmail.com>
2022-04-25 20:26:24 +0530

    complete

--- ajax-assg/assg.js
+++ ajax-assg/assg.js
@@ -1,4 +1,4 @@
-var data;
+var allData;
 
 var xhr = new XMLHttpRequest();
 var url = 'https://jsonplaceholder.typicode.com/users';
@@ -6,30 +6,81 @@ xhr.response = 'json';
 xhr.onreadystatechange = () => {
     if (xhr.readyState === XMLHttpRequest.DONE) {
         let json = JSON.parse(xhr.responseText);
-        data = json;
-        let ul = document.createElement('ol')
-        for (key in json) {
-
-            let li = document.createElement('li')
-            text = document.createTextNode(JSON.stringify(json[key].name))
-            // li.append(text)
-
-            let anchor = document.createElement('a');
-            anchor.setAttribute("href", `http://127.0.0.1:5500/ajax-assg/user.htm?name=`+json[key].name);
-            anchor.append(text)
-            li.append(anchor)
-
-            ul.appendChild(li)
-        }
-
-        document.getElementById('dataDiv').appendChild(ul)
+        allData = json;
+    
     }
 };
 xhr.open("GET", url, true);
 xhr.send()
 
+var clickCount = 0;
+function getFive(num) {
+    var data = allData;
+    document.getElementById('users').innerText = ""
+    if ((clickCount * num) >= data.length) {
+        clickCount = 0;
+    }
+    let startIndex = clickCount * num;
 
-function getFive() {
+    let endIndex = (clickCount + 1) * num
+    clickCount++;
+    console.log("clickCount: ", clickCount)
+    var ol = document.createElement('ol');
 
+    for (let i = startIndex; i < endIndex; i++) {
+
+        console.log(data[i].id);
+        let li = document.createElement('li')
+        text = document.createTextNode(JSON.stringify(data[i].name))
+
+        let anchor = document.createElement('a');
+        anchor.setAttribute("href", `http://127.0.0.1:5500/ajax-assg/user.htm?name=` + data[i].name);
+        anchor.append(text);
+        li.append(anchor);
+
+        ol.appendChild(li);
+    }
+
+    document.getElementById('users').appendChild(ol)
 }
 
+function getAll() {
+    var data = allData;
+    document.getElementById('users').innerText = ""
+
+    var ol = document.createElement('ol');
+
+    for (let i = 0; i < data.length; i++) {
+
+        console.log(data[i].id);
+        let li = document.createElement('li')
+        text = document.createTextNode(JSON.stringify(data[i].name))
+
+        let anchor = document.createElement('a');
+        anchor.setAttribute("href", `http://127.0.0.1:5500/ajax-assg/user.htm?name=` + data[i].name);
+        anchor.append(text);
+        li.append(anchor);
+
+        ol.appendChild(li);
+    }
+
+    document.getElementById('users').appendChild(ol)
+}
+
+function getDetailedUsers() {
+    console.log(allData)
+    document.getElementById('users').innerText = ""
+    var data = allData;
+    let ol = document.createElement('ol');
+    for (key in data) {
+
+        let li = document.createElement('li');
+        text = document.createTextNode(JSON.stringify(data[key]));
+
+        li.append(text);
+
+        ol.appendChild(li);
+    }
+
+    document.getElementById('users').appendChild(ol);
+}
